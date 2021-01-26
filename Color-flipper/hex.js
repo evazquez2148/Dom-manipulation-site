8f0edc93c8cabd947a6ee8f64b1ae292d1ced038
System Administrator <root@eliecels-imac.home>
2021-01-26 10:12:11 -0500

    fixed minor bug related to typo

--- Color-flipper/hex.js
+++ Color-flipper/hex.js
@@ -1,13 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const btn = document.querySelector("btn");
+const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   let hexColor = "#";
   for (index = 0; index < 6; index++) {
-    hexColor += hex[0];
+    hexColor += hex[getRandom()];
   }
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
 });
+
+function getRandom() {
+  return Math.trunc(Math.random() * hex.length);
+}
